a82ef9d939827ecd51016d39e23014c8ed72471e
KID <jyhsu2000@yahoo.com.tw>
2017-11-28 17:52:26 +0800

    Create Triangle

---
+++ src/main/java/club/kid7/Triangle.java
@@ -0,0 +1,18 @@
+package club.kid7;
+
+public class Triangle {
+    private double a;
+    private final double b;
+    private final double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public String getType() {
+        //TODO
+        return "";
+    }
+}
